7daf0421a0d302da3fd2c5c7ee40f8879651f6b4
Ricky <uhrishabh@gmail.com>
2023-06-21 15:04:32 +0200

    Added Utils and First state retrieval using BM25

--- first_stage_retrieval.py
+++ first_stage_retrieval.py
@@ -4,7 +4,7 @@ import pyterrier as pt
 import sys
 import re
 import string
-
+from utils import *
 # Set JAVA_HOME environment variable
 os.environ['JAVA_HOME'] = "/usr/lib/jvm/java-1.11.0-openjdk-amd64/"
 
@@ -29,27 +29,31 @@ def clean_en_text(text):
     text = remove_whitespaces(text)
     return text.strip().lower()
 
-dataset_path='/home/ubuntu/rupadhyay/CREDPASS'
 
-config={'TREC':{'file_path':f'''{dataset_path}/TREC2020_1M_labeled_clean.csv''',
-                'index_path':f'''{dataset_path}/trec2020_bm25''',
+
+if len(sys.argv)>3:
+    data_set=sys.argv[1] # TREC or CLEF
+    indexing=sys.argv[2] # True or False
+    dataset_path=sys.argv[3] # Path of the coped folder
+else:
+    data_set = 'TREC'
+    indexing = False
+    dataset_path='/home/ubuntu/rupadhyay/CREDPASS'
+
+config={'TREC':{'folder_path':f'''{dataset_path}/TREC''',
+                'file_path':f'''{dataset_path}/TREC/TREC2020_1M_labeled.csv''',
+                'index_path':f'''{dataset_path}/TREC/trec2020_bm25''',
                 'topics':f'''{dataset_path}/TREC/topics.csv''',
                 'result_name':f'''{dataset_path}/TREC/trec2020_BM25.csv''',
                 'qrels':f'''{dataset_path}/TREC/trec_qrels_top.csv''',
                 'final_retrieved_name':f'''{dataset_path}/TREC/TREC2020_BM25_clean_100.csv'''},
-        'CLEF':{'file_path':f'''{dataset_path}/Clef2020_1M_labeled_clean.csv''',
-                'index_path':f'''{dataset_path}/clef2020_bm25''',
-                'topics':f'''{dataset_path}/clef_topics.csv''',
+        'CLEF':{'folder_path':f'''{dataset_path}/CLEF''',
+                'file_path':f'''{dataset_path}/CLEF/Clef2020_1M_labeled.csv''',
+                'index_path':f'''{dataset_path}/CLEF/clef2020_bm25''',
+                'topics':f'''{dataset_path}/CLEF/clef_topics.csv''',
                 'result_name': f'''{dataset_path}/CLEF/clef2020_BM25.csv''',
                 'qrels': f'''{dataset_path}/CLEF/clef_qrels_top.csv''',
                 'final_retrieved_name': f'''{dataset_path}/CLEF/CLEF2020_BM25_clean_100.csv'''}}
-
-if len(sys.argv)>3:
-    data_set=sys.argv[1]
-    indexing=sys.argv[2]
-else:
-    data_set = 'TREC'
-    indexing = False
 
 if indexing:
     index_doc=pd.read_csv(config[data_set]['file_path'],sep='\t')
@@ -74,12 +78,13 @@ results['Q0']=0
 result=results[['qid','Q0','docno','rank','score']]
 results.to_csv(f'''{config[data_set]['result_name']}''',sep=' ', index=None, header=None)
 
+
 qrels_path=f'''{config[data_set]['qrels']}'''
 qrels = pt.io.read_qrels(qrels_path)
 eval = pt.Utils.evaluate(results,qrels,metrics=["ndcg"], perquery=True)
+print(eval)
 
 docs=pd.read_csv(config[data_set]['file_path'],sep='\t')
 docs=docs[['docno','text']]
 merged_results=pd.merge(results,docs,on=['docno'])
-merged_results.to_csv(f'''{config[data_set]['final_retrieved_name']}''',sep='\t',index=None)
-
+merged_results.to_csv(f'''{config[data_set]['final_retrieved_name']}''',sep='\t',index=False)
